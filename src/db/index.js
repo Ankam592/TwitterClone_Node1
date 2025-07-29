867f8e3651442a72371bdf6ec73bf48a66c5b1c0
Ankam592 <manojkumarankam0405@gmail.com>
2025-07-29 17:59:56 +0100

    clean dependencies

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     console.log(`🔄 Connecting to MongoDB..`);
+    mongoose.set('debug', true);
+
     const conn = await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=MongoDB`, {
     });
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
