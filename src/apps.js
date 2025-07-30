622424d160c302738cdd29f3a497d2d7f683b13c
Ankam592 <manojkumarankam0405@gmail.com>
2025-07-30 09:09:00 +0100

    serving static files issue

--- src/apps.js
+++ src/apps.js
@@ -34,10 +34,10 @@ app.use(cookieParser())            //serve assets like images or css to keep any
 app.use("/WeatherApp/uploads", express.static(path.join(__dirname, "../uploads")));
 
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get("/WeatherApp/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// app.get("/WeatherApp/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+// });
 // this middleware sets res.locals.user as current user role  and for every request this runs so wherever we go we will be having an idea who logged in and what content to print on html page 
 // eg: when we login we send isauthenticated information to ejs file by keeping in res.locals so we can check condition and print log out button or login button accordingly
 app.use((req, res, next) => {
